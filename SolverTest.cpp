b28206a22edfec3f416158602d2e9efaba7a2759
SkinnyRat <howard@flickps.com>
2016-06-26 15:46:57 +0800

    Update SolverTest.cpp

--- SolverTest.cpp
+++ SolverTest.cpp
@@ -1,48 +1,99 @@
 #include <iostream> 
 #include <sstream> 
-#include <cstring>
+#include <cstring> 
+#include <cstdio> 
 
 #include "TestHarness.h" 
 #include "Sudoku.h" 
 
-
-TEST(InputTest, CppUnitLite)
+TEST(IOTest, CppUnitLite)
 { 
-    std::stringstream Out, Out1, Out2; 
-    std::string Line, Message; 
+    std::stringstream Out; 
+    Sudoku S("Sample.txt", Out, true); 
     
     
-    // Default sample file where everything is expected to be OK. 
+    // Test for repeated digits on rows. 
     
-    Sudoku S("Sample.txt", Out, false); 
-    while(std::getline(Out, Line)) 
-    { 
-        if (Line.find("Count") != std::string::npos) 
-            Message = Line; 
-    } 
-    CHECK_EQUAL(Message, "Count = 14890"); 
+    std::vector<int>  Puzzle = 
+    {
+        0,0,0,0,0,0,0,0,0,
+        0,0,3,0,0,3,0,8,5,
+        0,0,1,0,2,0,0,0,0,
+        0,0,0,5,0,7,0,0,0,
+        0,0,4,0,0,0,1,0,0,
+        0,9,0,0,0,0,0,0,0,
+        5,0,0,0,0,0,0,7,3,
+        0,0,2,0,1,0,0,0,0,
+        0,0,0,0,4,0,0,0,9 
+    }; 
     
-    
-    // Sample file with incomplete lines or delimeters between characters. 
-    
-    Sudoku S1("SampleBad1.txt", Out1, true); 
-    S1.IOFile(Out1); 
-    while(std::getline(Out1, Line)) 
-    { 
-        if (Line.find("Line") != std::string::npos) 
-            Message = Line; 
-    } 
-    CHECK_EQUAL(Message, "Line 8 incomplete or mal-formatted."); 
+    S.SolverSane(Out, Puzzle); 
+    CHECK_EQUAL(Out.str(), "Error: Repeated '3's on Row 2.\n"); 
+    Out.str(""); 
     
     
-    // Sample file with excess data or otherwise no valid solution. 
+    // Test for repeated digits on columns. 
     
-    Sudoku S2("SampleBad2.txt", Out2, true); 
-    S2.IOFile(Out2); 
-    Out2.str(""); 
+    Puzzle = 
+    {
+        0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,3,0,8,5,
+        0,0,1,0,2,0,0,0,0,
+        0,0,0,5,0,7,0,0,0,
+        0,0,4,0,0,0,1,0,0,
+        0,9,0,0,4,0,0,0,0,
+        5,0,0,0,0,0,0,7,3,
+        0,0,2,0,1,0,0,0,0,
+        0,0,0,0,4,0,0,0,9 
+    }; 
     
-    S2.IOStats(Out2); 
-    CHECK_EQUAL(Out2.str(), "\nNo solution found.\n\n"); 
+    S.SolverSane(Out, Puzzle); 
+    CHECK_EQUAL(Out.str(), "Error: Repeated '4's on Col 5.\n"); 
+    Out.str(""); 
+    
+    
+    // Test for repeated digits in boxes. 
+    
+    Puzzle = 
+    {
+        0,0,0,0,0,0,0,0,0,
+        0,0,0,0,0,3,0,8,5,
+        0,0,1,0,2,0,0,0,0,
+        0,0,0,5,0,7,0,0,0,
+        0,0,4,0,0,0,1,0,0,
+        0,9,0,0,0,0,0,0,0,
+        5,0,0,4,0,0,0,7,3,
+        0,0,2,0,1,0,0,0,0,
+        0,0,0,0,4,0,0,0,9 
+    }; 
+    
+    S.SolverSane(Out, Puzzle); 
+    CHECK_EQUAL(Out.str(), "Error: Repeated '4's in Box 8.\n"); 
+    Out.str(""); 
+    
+    
+    // Test of final result based on sample valid solution. 
+    //   Taken from: https://projecteuler.net/problem=96 
+    
+    std::string After = 
+        "\nSolution: \n" \
+        " 483 921 657\n" \
+        " 967 345 821\n" \
+        " 251 876 493\n" \
+        "\n"             \
+        " 548 132 976\n" \
+        " 729 564 138\n" \
+        " 136 798 245\n" \
+        "\n"             \
+        " 372 689 514\n" \
+        " 814 253 769\n" \
+        " 695 417 382\n" ; 
+    
+    Sudoku T("SampleGood.txt", Out, false); 
+    Out.str(""); 
+    T.IOPrint(Out); 
+    CHECK_EQUAL(Out.str(), After); 
 }
+
 
 
